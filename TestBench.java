81defbfc5ae1bea130342749d33d8c988fd88fc5
jnagapavani27 <jnagapavani27@gmail.com>
2026-08-08 14:31:06 +0530

    Create TestBench.java

---
+++ TestBench.java
@@ -0,0 +1,18 @@
+public class TestBench {
+
+    public static void main(String[] args) {
+
+        System.out.println("Test Case 1: 3 rows");
+        PyramidPattern.printPyramid(3);
+
+        System.out.println();
+
+        System.out.println("Test Case 2: 5 rows");
+        PyramidPattern.printPyramid(5);
+
+        System.out.println();
+
+        System.out.println("Test Case 3: 1 row");
+        PyramidPattern.printPyramid(1);
+    }
+}
